42742b123e8dc5fe7b22a8c182007a72bdf31cf6
Prompt Kerdphoksup <promptkp@mit.edu>
2023-09-10 15:57:00 -0400

    add source code link

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -6,6 +6,8 @@ const Bottom = () => {
       <div>·</div>
       <a className='dog' href='https://random.dog/' target='_blank' rel='noreferrer noopener'>click me!</a>
       <div>·</div>
+      <a className='source-code' href='https://github.com/promptkp7/typer/tree/master' target='_blank' rel='noreferrer noopener'>source code</a>
+      <div>·</div>
       <a className='description' href='https://monkeytype.com/' target='_blank' rel='noreferrer noopener'>inspired by monkeytype.com</a>
     </div>
   )
